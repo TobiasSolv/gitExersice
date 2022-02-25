f6c8663e009094e243251a3260a094d4b3891436
Tobias <tobiasurban@hotmail.com>
2022-02-25 09:31:33 +0100

    added greet

--- src/GitExercise.java
+++ src/GitExercise.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class GitExercise {
 
     void go(){
+        String userinput;
         System.out.println("It works!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        userinput = in.nextLine();
+        System.out.println("Hello, " + userinput);
 
     }
 
