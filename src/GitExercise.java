d9ae217367dc004fbc3d0fc5264215f14e3fbc90
Tobias <tobiasurban@hotmail.com>
2022-02-25 09:36:25 +0100

    added askAboutAge

--- src/GitExercise.java
+++ src/GitExercise.java
@@ -4,11 +4,17 @@ public class GitExercise {
 
     void go(){
         String userinput;
-        System.out.println("It works!");
+        int userinput2;
         Scanner in = new Scanner(System.in);
+        System.out.println("It works!");
         System.out.print("Enter your name: ");
         userinput = in.nextLine();
         System.out.println("Hello, " + userinput);
+
+        System.out.println();
+        System.out.print("What is your age? ");
+        userinput2 = in.nextInt();
+        System.out.println("Your age is, " + userinput2);
 
     }
 
