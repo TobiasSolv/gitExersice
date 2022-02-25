8eec344faf7c0f77ee98150d57ef17a63718fc49
Tobias <tobiasurban@hotmail.com>
2022-02-25 10:42:53 +0100

    new name

--- src/GitExercise.java
+++ src/GitExercise.java
@@ -6,7 +6,9 @@ public class GitExercise {
         String userinput;
         int userinput2;
         Scanner in = new Scanner(System.in);
+
         System.out.println("It works!");
+
         System.out.print("Enter your name: ");
         userinput = in.nextLine();
         System.out.println("Hello, " + userinput);
@@ -15,6 +17,8 @@ public class GitExercise {
         System.out.print("What is your age? ");
         userinput2 = in.nextInt();
         System.out.println("Your age is, " + userinput2);
+
+        System.out.println("something");
 
     }
 
