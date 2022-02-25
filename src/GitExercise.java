661c46f0bd28460737424e7db4db9dd3fcf77634
Tobias <tobiasurban@hotmail.com>
2022-02-25 09:29:34 +0100

    initial commit

---
+++ src/GitExercise.java
@@ -0,0 +1,11 @@
+public class GitExercise {
+
+    void go(){
+        System.out.println("It works!");
+
+    }
+
+    public static void main(String[] args) {
+        new GitExercise().go();
+    }
+}
